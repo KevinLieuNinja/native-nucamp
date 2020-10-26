b9a4a9d3a2c963cd8cfbede9f5cc92021dcaa99a
Kevin Lieu Git <klieu925@gmail.com>
2020-10-26 16:56:06 -0700

    swipe and delete

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -185,3 +185,8 @@ export const addFavorite = (campsiteId) => ({
   type: ActionTypes.ADD_FAVORITE,
   payload: campsiteId,
 });
+
+export const deleteFavorite = (campsiteId) => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId,
+});
